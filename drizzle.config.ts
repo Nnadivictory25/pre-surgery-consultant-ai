ae4b6484a6662323e789aab24a28d3cad874bfe6
Nnadi Victory <chikamagozi25@gmail.com>
2025-12-02 20:52:03 +0100

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'drizzle-kit';
 
 const databaseUrl = process.env.DATABASE_URL || './chatbot.db';
 
+console.log('databaseUrl', databaseUrl);
+
 export default defineConfig({
 	dialect: 'sqlite', // 'mysql' | 'sqlite' | 'turso'
 	schema: './src/lib/server/db/schema.ts',

481a2f82969f573fac4fedfe5b6817e428048f77
Nnadi Victory <chikamagozi25@gmail.com>
2025-12-02 20:58:13 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// Precompress static assets
+			precompress: false // Disable for faster builds
+		})
 	}
 };
 
